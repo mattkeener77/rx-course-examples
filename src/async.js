6259e9f69616f15d6dfd7acad0d87571d4d5f96f
Matt Keener <mattkeener@Matts-iMac.local>
2019-07-20 20:52:39 -0400

    end

--- src/async.js
+++ src/async.js
@@ -1,1 +1,25 @@
 import { add } from './helpers';
+import { resolve } from 'path';
+
+add.li('line 3');
+
+async function runPromise() {
+    add.li('line 7')
+    const p = new Promise(
+        (resolve, reject) => {
+            setTimeout(
+                ()=>{
+                    resolve('We are complete!');
+                }, 5000
+            )
+        }
+    );
+    //const message = await p;
+    add.li('line 18')
+}//end of runPromise
+
+
+runPromise()
+
+
+add.li('line 25')
