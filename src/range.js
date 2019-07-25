d06387b058c207bf7a5600cf0536d7af1cc29b01
Matt Keener <mattkeener@Matts-iMac.local>
2019-07-24 20:10:18 -0400

    start

--- src/range.js
+++ src/range.js
@@ -1,14 +1,6 @@
 import {add} from './helpers';
-import {range, of} from 'rxjs';
-import {delay, concatMap} from 'rxjs/operators';
+import {range} from 'rxjs';
 
 
 
-const numbers = range(50, 51);
 
-
-numbers.pipe(
-    concatMap(
-        (value) => of(value).pipe(delay(1000))
-    )
-).subscribe(add.li);
