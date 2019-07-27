2e1f65505b90178be9ba6dc08f8322c8347e16c5
Matt Keener <matthewkeener@me.com>
2019-07-27 08:49:44 -0400

    start

--- src/tap.js
+++ src/tap.js
@@ -1,14 +1,3 @@
 import {add} from "./helpers";
-import {interval} from "rxjs";
-import {tap, map, take } from "rxjs/operators";
+import {} from "rxjs/operators";
 
-const counter = interval(1000);
-
-counter.pipe(
-    take(10),  
-    map(x=>Math.pow(x, 2)),
-    tap(
-        (value)=> add.li('value from line 11: ' + value)
-    ),
-    map(x=>Math.sqrt(x))
-).subscribe(add.li)
