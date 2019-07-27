cad6f02528d1dfccc705428adb1e7fc38978d1f7
Matt Keener <matthewkeener@me.com>
2019-07-27 09:04:52 -0400

    start

--- src/skip.js
+++ src/skip.js
@@ -1,18 +1,5 @@
 import {add} from './helpers'
-import {from, fromEvent, interval} from 'rxjs';
-import {take, skip, skipWhile, skipLast, skipUntil} from 'rxjs/operators';
+import {from, fromEvent} from 'rxjs';
+import {take, skip, skipWhile, skipLast} from 'rxjs/operators';
 
 
-from(['apples', 'grapes', 'oranges', 'pears'])
-.pipe(
-    skipLast(2)
-)
-
-const button = document.getElementById('submit');
-const buttonEvents = fromEvent(button, 'click');
-
-interval(1000)
-.pipe(
-    skipUntil(buttonEvents)
-)
-.subscribe(add.li);
