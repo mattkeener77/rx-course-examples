edd963835215dd0423474039914410624a8ba426
Matt Keener <matthewkeener@me.com>
2019-07-27 11:02:26 -0400

    end

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './'
+import './concat'
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    document.getElementById('title').innerHTML = "";
+    document.getElementById('title').innerHTML = "concat()";
      
 });
