85b57fc25f58533c19a5162d7c500465e35ccd87
Matt Keener <matthewkeener@me.com>
2019-07-27 09:21:12 -0400

    start

--- src/scan-reduce.js
+++ src/scan-reduce.js
@@ -2,22 +2,3 @@ import {add} from "./helpers";
 import {interval, from} from "rxjs";
 import {scan,reduce, take, concatMap} from "rxjs/operators";
 
-
-
-const FS = interval(100).pipe(
-    take(20),
-    reduce(
-        (acc,value)=>{
-            const n = value + 1;
-            const last = acc[n];
-            const beforeLast = acc[n-1];
-
-            return [...acc, last + beforeLast]
-        }, [0,1]
-    )
-)
-
-FS.pipe(
-    concatMap(sequence => from(sequence))
-)
-.subscribe(add.li);
