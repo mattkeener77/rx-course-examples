fd2d1c1c4f566014446577326b11fd2d3b459a56
Matt Keener <matthewkeener@me.com>
2019-07-27 11:00:21 -0400

    start

--- src/merge.js
+++ src/merge.js
@@ -2,13 +2,3 @@ import {add} from './helpers';
 import { merge, interval, fromEvent} from 'rxjs';
 import {take, map} from 'rxjs/operators';
 
-const button = document.getElementById('submit');
-const streamOne = interval(1000).pipe(take(10));
-const streamTwo = fromEvent(button, 'click').pipe(
-    map(
-        (e)=> 'clicked'
-    )
-);
-
-
-merge(streamOne, streamTwo).subscribe(add.li);
