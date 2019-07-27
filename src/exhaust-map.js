ae490e3370bc7e58362fbc809ce3079b363d8cfa
Matt Keener <matthewkeener@me.com>
2019-07-27 11:19:47 -0400

    end

---
+++ src/exhaust-map.js
@@ -0,0 +1,59 @@
+import { add, animate } from "./helpers";
+import { fromEvent, interval } from "rxjs";
+import { 
+    exhaustMap, 
+    switchMap, 
+    mergeMap,
+    map,
+    take
+} from "rxjs/operators";
+
+// Projects each source value to an Observable which is merged
+// in the output Observable only if the previous projected
+// Observable has completed.
+
+const startButton = document.getElementById("start");
+const startClicked = fromEvent(startButton, "click");
+const circle = document.getElementById("circle");
+
+
+
+// startClicked
+//     .pipe(
+//         mergeMap(
+//             () => {
+//                 return animate(5000)
+//             }
+//         )
+//     ).subscribe(
+//         (t) => {
+//             circle.style.marginLeft = `${t*450}px`
+//         }
+//     )
+
+
+interval(2000)
+    .pipe(
+        take(3),
+        map(
+            value => `${value * 100}`
+        ),
+        exhaustMap(
+            x => {
+                console.log(`Source: ${x}`) 
+                // This generated Observable has to complete
+                // Before any value to the source is listened to.
+                // If the values from the source complete before the 
+                // generated Observable, they will be ignored.
+                return interval(1000).pipe(
+                    take(5),
+                    map(
+                        value => `inner(${value}), outer(${x})`
+                    )
+                )
+            }
+        )
+    )
+    .subscribe(
+        value => add.li(`Emitted Value: ${value}`)
+    );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './'
+import './exhaust-map'
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    document.getElementById('title').innerHTML = "";
+    document.getElementById('title').innerHTML = "exhaustMap()";
      
 });
