076024da8b0967c3342f4c17f65e6a3ef3891721
Matt Keener <matthewkeener@me.com>
2019-07-27 11:16:09 -0400

    start

--- src/switch-map.js
+++ src/switch-map.js
@@ -9,9 +9,6 @@ import {
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const moves = fromEvent(canvas, 'mousemove');
-const down = fromEvent(canvas, 'mousedown');
-const up = fromEvent(canvas, 'mouseup');
 
 function brush (coords){
     context.lineWidth = 5;
@@ -19,28 +16,3 @@ function brush (coords){
     context.stroke();
 }
 
-down.pipe(
-    tap(
-        (evt)=>{
-            context.strokeStyle = "purple";
-            context.beginPath();
-            context.moveTo(evt.offsetX, evt.offsetY);
-        }
-    ),
-    switchMap(
-        (evt) => moves.pipe(
-            map(
-                evt=>{
-                    return { x: evt.offsetX, y: evt.offsetY }
-                }
-            ),
-            takeUntil(
-                up
-            )
-        )
-    )
-).subscribe(
-    (coords)=>{
-        brush(coords);
-    }
-)
